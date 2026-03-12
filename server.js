d532bbbec3a5c8ec2717fe1b31f20bd84d12ed97
Dileep Kumar <chintaladileep968@gmail.com>
2026-03-12 09:25:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -244,8 +244,8 @@ app.get("/logout", (req, res) => {
 });
 
 // ================= SERVER =================
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log("Server running on port " + PORT);
 });
